3f415568ba0b336cddd72730737c9b41e16d7545
Kenn Takara <kenntakara@hotmail.com>
2014-09-27 04:15:19 +0900

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='pysolve',
-    version='0.1.6',
+    version='0.1.7',
     packages=find_packages(),
     install_requires=['sympy', 'numpy'],
     license='MIT',
